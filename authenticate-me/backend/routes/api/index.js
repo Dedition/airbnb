6ad8f956de7b9748a2db1cadefdc26997441bfe5
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-04-29 01:05:53 -0400

    Login working

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const asyncHandler = require('express-async-handler');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
 router.post('/test', function (req, res) {
     res.json({ requestBody: req.body });
@@ -36,5 +38,13 @@ router.get(
         return res.json(req.user);
     }
 );
+
+
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+    res.json({ requestBody: req.body });
+});
 
 module.exports = router;
