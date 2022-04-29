26995d82ca20ef4b8b9eb9b1d8669a776f2ff966
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-04-29 16:29:43 -0400

    Added header on Header component

---
+++ authenticate-me/frontend/src/components/Header/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Header.css';
+
+// https://images.contentstack.io/v3/assets/bltb428ce5d46f8efd8/bltc1cd16d484367ad6/6058dd2a0f19b910ce5f9fd5/Group_1_(1).png?crop=100p,100p,x0,y0&width=720&height=405&auto=webp
+function Header() {
+    return (
+        <div className='header'>
+            <img className='header_icon' src={process.env.PUBLIC_URL + `/images/svgexport-1.png`} alt=""
+            />
+
+            <div className='header_center'>
+                <input type="text" />
+                <i className="fa-solid fa-magnifying-glass" />
+            </div>
+
+            <div className='header_right'>
+                <a href="" className='header_right_text'>Become A Host</a>
+                <i className="fa-solid fa-globe" />
+                <i className='fa-solid fa-bars' />
+                <i className="fa-solid fa-user-circle" />
+            </div>
+        </div >
+    )
+}
+
+export default Header
