8b4203f6246c929a1686beb29227bde6b6b69712
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-05-03 10:08:33 -0400

    Fixed issue with list rendering. Didn't like that due to object destructuring. Previous cause for concerns fixed. Added 4 files for spots.

--- frontend/src/components/SpotsPage/DeleteSpot.js
+++ frontend/src/components/SpotsPage/DeleteSpot.js
@@ -1,61 +1,22 @@
-import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, NavLink, useHistory } from "react-router-dom";
-import {
-    Button, Card, CardBody, CardHeader, CardTitle, Col, Form, FormGroup, Input, Label, Row
-} from "reactstrap";
 import * as spotActions from "../../store/spots";
+import { Button } from "reactstrap";
 
-// Delete a spot that a user created
-
-const DeleteSpot = () => {
+const DeleteSpot = ({ spotId }) => {
     const dispatch = useDispatch();
     const history = useHistory();
-    const [spotId, setSpotId] = useState("");
-    const [isLoaded, setIsLoaded] = useState(false);
+    const sessionUser = useSelector(state => state.session.user);
 
-    const userId = useSelector(state => state.session.user.id);
-    const spot = useSelector((state) => state.spots.spot);
-    const error = useSelector((state) => state.spots.error);
-    // console.log(typeof userId);
-
-    const handleSubmit = (e) => {
+    const handleDelete = async (e) => {
         e.preventDefault();
-
-        dispatch(spotActions.removeSpot({
-            userId,
-            spotId
-        }));
-
-        history.push("/listings");
+        const deletedSpot = await dispatch(spotActions.removeSpot(spotId));
+        if (deletedSpot) return history.push("/listings");
     };
-
-    useEffect(() => {
-        if (spot) {
-            setSpotId(spot.id);
-            setIsLoaded(true);
-        }
-    }, [spot]);
 
     return (
         <div>
-            <Card>
-                <CardHeader>Delete Spot</CardHeader>
-                <CardBody>
-                    <Form onSubmit={handleSubmit}>
-                        <Row>
-                            <Col>
-                                <FormGroup>
-                                    <Label for="spotId">Spot Id</Label>
-
-                                    <Input type="text" name="spotId" id="spotId" placeholder="Spot Id" value={spotId} onChange={(e) => setSpotId(e.target.value)} />
-                                </FormGroup>
-                            </Col>
-                        </Row>
-                        <Button type="submit">Delete Spot</Button>
-                    </Form>
-                </CardBody>
-            </Card>
+            <Button color="danger" onClick={handleDelete}>Delete Spot</Button>
         </div>
     );
 };
