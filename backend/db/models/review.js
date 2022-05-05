998aaac43cac4027841478ddcaffe70a05ba0a22
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-05-05 15:37:43 -0400

    Pushed to main and P is back to being broken. Go figure. Added scopes, associations and methods for reviews.

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -1,13 +1,72 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Review = sequelize.define('Review', {
-    userId: DataTypes.INTEGER,
-    spotId: DataTypes.INTEGER,
-    review: DataTypes.STRING,
-    rating: DataTypes.INTEGER
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: 'Users' }
+    },
+    spotId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: 'Properties' }
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    cleanliness: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    communication: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  }, {
+    scopes: {
+      reviewDataOnly: {
+        attributes: { exclude: ['userId', 'content'] }
+      },
+      reviewContentOnly: {
+        attributes: { exclude: ['spotId', 'rating', 'communication', 'checkIn', 'cleanliness'] }
+      }
+    }
   }, {});
+
+  Review.createReview = async (reqData) => await Review.create(reqData);
+
+  Review.getReviewsBySpotId = async (id) => await Review.findAll({
+    where: { spotId }, order: [['createdAt', 'DESC']]
+  });
+
+  Review.getReviewsByUserId = async (spotId) => await Review.findByPk(spotId);
+
+  Review.updateReview = async (details) => {
+    const id = details.id
+    delete details.id;
+    await Review.update(details, {
+      where: { id },
+      returning: true,
+      plain: true
+    });
+    return await Review.findByPk(id);
+  };
+
+  Review.deleteReview = async (id) => {
+    const review = await Review.findByPk(id);
+    if (!review) throw new Error('Review not found. :( Be the first!');
+    await Review.destroy({ where: { id } });
+    return review;
+  }
+
   Review.associate = function (models) {
     // associations can be defined here
+    Review.belongsTo(models.User, { foreignKey: "userId" });
     Review.belongsTo(models.Spot, {
       foreignKey: 'spotId',
       onDelete: 'CASCADE'
@@ -15,3 +74,8 @@ module.exports = (sequelize, DataTypes) => {
   };
   return Review;
 };
+
+// userId: DataTypes.INTEGER,
+// spotId: DataTypes.INTEGER,
+// review: DataTypes.STRING,
+// rating: DataTypes.INTEGER
