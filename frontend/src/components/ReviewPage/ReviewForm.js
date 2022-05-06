315acfe540b6945d3c047d7e11074684b7ad6c7a
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-05-06 01:49:22 -0400

    Added ReviewForm.js file with a form. Passing down props from Form/Form.js

---
+++ frontend/src/components/ReviewPage/ReviewForm.js
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+import { createReview, getReviews } from "../../store/review";
+import { NewForm, NumInput } from '../Form';
+
+const ReviewForm = ({ closeModal }) => {
+    const dispatch = useDispatch();
+    const { id } = useParams();
+    const userId = useSelector(state => state.session.user.id);
+
+    useEffect(() => { dispatch(getReviews(id)) }, [dispatch, id]);
+
+    const [content, setContent] = useState('');
+    const [cleanliness, setCleanliness] = useState(0);
+    const [communication, setCommunication] = useState(0);
+    const [rating, setRating] = useState(0);
+    const [errors, setErrors] = useState({});
+    const [validationErrors, setValidationErrors] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const newReview = await dispatch(createReview({
+            userId, id, content, cleanliness, communication, rating
+        })).catch(async (res) => {
+            const data = await res.json();
+            if (data && data.errors) setErrors(data.errors);
+        });
+
+        if (newReview?.errors) setErrors(newReview?.errors);
+
+        return closeModal();
+    }
+
+    useEffect(() => {
+        const errors = [];
+        if (content.length < 1) errors.push('Review content is required');
+        if (cleanliness < 1 || communication < 1 || rating < 1) errors.push(`Sorry, but ${e.target.value} rating must be between 1 and 5`);
+        setValidationErrors(errors);
+    }, [content, cleanliness, communication, rating]);
+
+    return (
+        <NewForm onSub={handleSubmit} validationErrors={validationErrors} errors={errors} buttonName={'Post'}>
+            <ul>
+                <li><label htmlFor='content'>Review Content</label></li>
+                <li><textarea name='content' id='content' value={content} onChange={(e) => setContent(e.target.value)} placeholder="Review" /></li>
+            </ul>
+            <NumInput min={1} name='Cleanliness' state={cleanliness} setState={setCleanliness} required={false} />
+            <NumInput min={1} name='Communication' state={communication} setState={setCommunication} required={false} />
+            <NumInput min={1} name='Rating' state={rating} setState={setRating} required={false} />
+        </NewForm>
+    )
+}
+
+export default ReviewForm;
