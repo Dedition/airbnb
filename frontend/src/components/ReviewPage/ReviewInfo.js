18f2ffca6e9a066a65819268c3b6f00659c6c3d0
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-05-06 02:45:17 -0400

    Added and filled reviewInfo file. Gained an error. The error is due to your Review model in the backend. The error disallows you from deleting a spot because the 'key id matches up to [a] userId'. onDelete: CASCADE not working. Revisit after reviews done.

--- frontend/src/components/ReviewPage/ReviewInfo.js
+++ frontend/src/components/ReviewPage/ReviewInfo.js
@@ -1,7 +1,52 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import ReviewModal from "./ReviewModal";
 
 // TODO ——————————————————————————————————————————————————————————————————————————————————
 // TODO                               Add modal form and CSS here
 // TODO ——————————————————————————————————————————————————————————————————————————————————
+
+const Meter = ({ rating }) => <meter value={rating} min={0} max={5} />
+
+const ReviewInfo = ({ reviewInfo, totalReviews, reviews }) => {
+
+    const [id] = useParams();
+    const [userReview, setUserReview] = useState(false);
+
+    const spot = useSelector(state => state.spot[id]);
+    console.log(spot)
+    const user = useSelector(state => state.session.user);
+    console.log(user)
+
+    useEffect(() => {
+        if (user) setUserReview(true);
+        else setUserReview(false);
+    }, [user, spot])
+
+    const ReviewInfoLine = ({ datum }) => (
+        <div className='review-meter' >
+            <strong>{`${datum.name}:`}</strong>
+            {!datum.value ? <div>WHY IS THERE NO REVIEWS?!</div> :
+                <>
+                    <Meter rating={datum.value} />
+                    <strong>{datum.value}</strong>
+                </>
+            }
+        </div>
+    );
+
+    return (
+        <div className='review_heading'>
+            {!reviewInfo[0]?.value ? <div>STILL NO REVIEWS???</div> :
+                <h2><i className="review-star fas fa-star" />{`${reviewInfo[0]?.value} • ${totalRevieews} reviews`} </h2>
+            }
+            <div className='review-meter-container'>
+                {reviewInfo.slice(1).map(datum => (<ReviewInfoLine key={datum.name} datum={datum} />))}
+            </div>
+            {!user && (<ReviewModal reviews={reviews} />)}
+        </div>
+    )
+}
+
+export default ReviewInfo;
