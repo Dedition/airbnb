61e637d8080ecc7fae1ecbad21e55166cff93993
Leone, Mark A [LGS] <mleone@lgsinnovations.com>
2022-05-08 22:59:47 -0400

    ALmost there

--- backend/routes/api/images.js
+++ backend/routes/api/images.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
-const imageUrls = require('../../assets/images/image_urls.json');
+const imageUrls = require('../../assets/spot-images/imageurls.json');
 
-router.get("/:id", asyncHandler(async (req, res) => {
-    const images = JSON.parse(imageUrls);
-    return images[id].url;
+const router = express.Router();
+
+router.get("/", asyncHandler(async (req, res) => {
+    // console.log(imageUrls)
+    // const images = JSON.parse(imageUrls);
+    // console.log(`Returning ${imageUrls[parseInt(req.params.id)]}`);
+    console.log('Returning image URLs')
+    return res.json(imageUrls);
 }));
 
 module.exports = router;
